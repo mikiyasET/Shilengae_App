221e4b0c2b0084f78146e2c3987372bb24334d4e
franol-geleta <franolgeleta5@gmail.com>
2021-01-13 10:25:14 +0300

    dark theme color fix

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -11,10 +11,10 @@ ThemeData basicTheme() {
 }
 
 ThemeData darkTheme() {
-  final ThemeData base = ThemeData.light();
+  final ThemeData base = ThemeData.dark();
   return base.copyWith(
     textSelectionColor: Color(0xffffffff),
-    primaryColor: Color(0xffffff), // red
+    primaryColor: Color(0xffE33134), // red
     secondaryHeaderColor: Color(0xff000000), // black
     accentColor: Color(0xff686866), // grey
   );
