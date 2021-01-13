dc8d8af70335731b710c4ee1cdb7ce03f65f08ed
Mikiyas Lemlemu <Mikiyaslemlemu@gmail.com>
2021-01-14 01:59:46 +0300

    Dark Mode  - Minor Color Fix

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData basicTheme() {
   final ThemeData base = ThemeData.light();
   return base.copyWith(
-    textSelectionColor: Color(0xffffffff),
+    textSelectionColor: Color(0xffffffff), // white
     primaryColor: Color(0xffE33134), // red
     secondaryHeaderColor: Color(0xff000000), // black
     accentColor: Color(0xff686866), // grey
@@ -13,9 +13,9 @@ ThemeData basicTheme() {
 ThemeData darkTheme() {
   final ThemeData base = ThemeData.dark();
   return base.copyWith(
-    textSelectionColor: Color(0xffffffff),
+    textSelectionColor: Color(0xffffffff), // white
     primaryColor: Color(0xffE33134), // red
-    secondaryHeaderColor: Color(0xff000000), // black
-    accentColor: Color(0xff686866), // grey
+    secondaryHeaderColor: Color(0xffffffff), // white
+    accentColor: Color(0xfff7f7f7), // light
   );
 }
